dd0235e138c1803ceba5ad375d8f18b7c16debb1
Bruno Vidal <brunokasprik@gmail.com>
2022-12-03 04:59:08 -0300

    css

--- src/App.js
+++ src/App.js
@@ -3,24 +3,36 @@ import "./App.css";
 function App() {
   const categories = [
     {
-      title: 'hats',
-      id: 'hats'
-      
-    }
-  ]
+      title: "Hats",
+      id: 1,
+    },
+    {
+      title: "Jackets",
+      id: 2,
+    },
+    {
+      title: "Sneakers",
+      id: 3,
+    },
+    {
+      title: "Womens",
+      id: 4,
+    },
+    {
+      title: "Mens",
+      id: 5,
+    },
+  ];
   return (
-    
-      <div className="categories-container">
-      {categories.map(({title}) => (
-          <div className="category-container">
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="background-image"></div>
           <h2>hats</h2>
           <p>Shop Now</p>
         </div>
-        ))}
-        
-      </div>
-      
-    
+      ))}
+    </div>
   );
 }
 
