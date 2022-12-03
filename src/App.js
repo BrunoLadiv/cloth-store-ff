b7503b1883c235acfb31a7e49085bad130ae86f2
Bruno Vidal <brunokasprik@gmail.com>
2022-12-03 04:46:16 -0300

    in setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const categories = [
+    {
+      title: 'hats',
+      id: 'hats'
+      
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="categories-container">
+      {categories.map(({title}) => (
+          <div className="category-container">
+          <h2>hats</h2>
+          <p>Shop Now</p>
+        </div>
+        ))}
+        
+      </div>
+      
+    
   );
 }
 
