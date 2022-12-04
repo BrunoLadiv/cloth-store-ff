90f5bad499ff8c327b9169310df1b9dc215260d2
Bruno Vidal <brunokasprik@gmail.com>
2022-12-04 04:06:43 -0300

    firebase configs

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,17 @@
+import { signInWithGooglePopup } from "../../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-    return ( <div>
-        <h1>sign in page</h1>
-    </div> );
+    const logGoogleUser = async () => { 
+        const response = await signInWithGooglePopup()
+        console.log(response)
+    }
+    return (
+        <div>
+            <h1>Sign in Page</h1>
+            <button onClick={logGoogleUser}>Sign in with Google</button>
+        </div>
+    )
+            
 }
  
 export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import {getFirestore,doc,getDoc,setDoc } from 'firebase/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0nx1WBYISs2klkjL_CCsQCVeAL-zAX_8",
+  authDomain: "clothing-store-ff-db.firebaseapp.com",
+  projectId: "clothing-store-ff-db",
+  storageBucket: "clothing-store-ff-db.appspot.com",
+  messagingSenderId: "39318683392",
+  appId: "1:39318683392:web:001e8dd967a1c4d08abde5"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider()
+provider.setCustomParameters({
+  prompt: "select_account"
+})
+
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore() 
+
+
