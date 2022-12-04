eb77b605b5baf0c08ed090cd70c7d8cf582434a6
Bruno Vidal <brunokasprik@gmail.com>
2022-12-04 02:17:44 -0300

    firebase added

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,17 +1,22 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrwLogo } from '../../../assets/086 crown.svg'
+import './navigation.styles.scss'
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
-        <Link className="logo-container" to='/'>
+              <Link className="logo-container" to='/'>
+                  <CrwLogo className="logo"/>
           
-          <div>logo</div>
         </Link>
-        <div className="links-container-container">
+        <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN 
           </Link>
         </div>
       </div>
