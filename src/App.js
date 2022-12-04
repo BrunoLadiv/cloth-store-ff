9bb4979c4aebe1787412369f182bedd8136328ca
Bruno Vidal <brunokasprik@gmail.com>
2022-12-04 01:28:21 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Home from "./components/routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/routes/navigation/navigation.component";
+
+ 
+const Shop = () => {
+  return ( 
+      <h1>I am the shop</h1>
+   
+    )};
+
+ 
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Navigation />} >
+            <Route index={true} element={<Home />} />
+            <Route path='shop' element={<Shop />} />
+      
+      </Route>
        
      
     </Routes>
